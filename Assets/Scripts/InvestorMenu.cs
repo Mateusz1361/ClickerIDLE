8461d7ce1faeb8c1ca5675130b67df343040834e
Mateusz1361 <ml307892@student.polsl.pl>
2024-07-27 17:05:42 +0200

    staring something

--- Assets/Scripts/InvestorMenu.cs
+++ Assets/Scripts/InvestorMenu.cs
@@ -1,11 +1,30 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InvestorMenu : MonoBehaviour {
     [SerializeField]
     private Button closeButton;
+    [SerializeField]
+    private TMP_Text investorsText;
 
+    private EquipmentMenu data;
+    
+    private ulong investors = 0;
     private void Awake() {
         closeButton.onClick.AddListener(() => gameObject.SetActive(false));
+        
+    }
+    public void OnEnable() {
+        CountingInvestors();
+    }
+    public void CountingInvestors()
+    {
+        if (data.Stone.Count >= 10)
+        {
+            Rational rational = data.Stone.Count - 10*investors;
+            investors = (ulong)rational; 
+            investorsText.text = investors.ToString();
+        }
     }
 }
